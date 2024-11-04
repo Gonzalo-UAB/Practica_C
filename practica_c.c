0f411f272106753a8908dc6ea099c36e0fb5e151
HectorBerger <hectorbergermartinez@gmail.com>
2024-11-04 09:47:57 +0100

    Multiplicació matriu per vector

--- practica_c.c
+++ practica_c.c
@@ -1,4 +1,4 @@
-w#include <stdio.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
@@ -8,7 +8,6 @@ w#include <stdio.h>
 float Mat[N][N];
 float MatDD[N][N];
 float V1[N], V2[N], V3[N], V4[N];
-
 
 void InitData(){
 int i,j;
@@ -33,8 +32,7 @@ for( i = 0; i < N; i++ ){
 //Imprimir vector
 void PrintVect( float vect[N], int from, int numel ) {
 	int final=((from+numel));
-	int i;
-	for (i=from;i<final;i++){
+	for (int i=from;i<final;i++){
         	printf("%f ", vect[i]);
 	}
 	printf("\n");
@@ -43,8 +41,7 @@ void PrintVect( float vect[N], int from, int numel ) {
 //Imprimir fila matriu
 void PrintRow( float mat[N][N], int row, int from, int numel ){
 	int final=((from+numel));
-        int i;
-        for (i=from;i<final;i++){
+        for (int i=from;i<final;i++){
                 printf("%f ", mat[row][i]);
 	}
 	printf("\n");
@@ -52,8 +49,7 @@ void PrintRow( float mat[N][N], int row, int from, int numel ){
 
 //Multiplicació escalar
 void MultEscalar( float vect[N], float vectres[N], float alfa ){
-    int i;
-    for (i=0;i<N;i++){
+    for (int i=0;i<N;i++){
         vectres[i] = vect[i]*alfa;
     }
 }
@@ -71,10 +67,9 @@ float Scalar( float vect1[N], float vect2[N] ){
 
 //Magnitud de un vector
 float Magnitude( float vect[N] ){
-    int i;
     float suma = 0;
     float res;
-    for (i=0;i<N;i++){
+    for (int i=0;i<N;i++){
         suma = suma+pow(vect[i],2);
     }
     res = sqrt(suma);
@@ -99,12 +94,10 @@ MultEscalar(vect1,vectres,magni);
 
 //Infini-norma
 float Infininorm( float M[N][N] ) {
-	int i;
-	int j;
 	float max;
-	for (i=0;i<N;i++){
+	for (int i=0;i<N;i++){
 		float suma = 0;
-		for (j=0;j<N;j++){
+		for (int j=0;j<N;j++){
 			suma += fabs(M[i][j]);
 		}
 		if ((i == 0) || (max < suma)){
@@ -116,12 +109,10 @@ float Infininorm( float M[N][N] ) {
 
 //Norma-ú
 float Onenorm( float M[N][N] ) {
-        int i;
-        int j;
         float max;
-        for (i=0;i<N;i++){
+        for (int i=0;i<N;i++){
                 float suma = 0;
-                for (j=0;j<N;j++){
+                for (int j=0;j<N;j++){
                         suma += fabs(M[j][i]);
                 }
                 if ((i == 0) || (max < suma)){
@@ -133,19 +124,30 @@ float Onenorm( float M[N][N] ) {
 
 //Norma de Frobenius
 float NormFrobenius( float M[N][N] ) {
-        int i;
-        int j;
-	float res;
 	float suma = 0;
-        for (i=0;i<N;i++){
-                for (j=0;j<N;j++){
-                        suma += (M[i][j])*(M[i][j]);
-                }
-        }
-	res = sqrt (suma);
-        return res;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+                    suma += (M[i][j])*(M[i][j]);
+            }
+    }
+        return  sqrt(suma);
 }
 
+//Dominant
+int DiagonalDom( float M[N][N] ){
+	int dom = 0;
+
+	return dom;
+}
+
+//Multiplicació d’una matriu per un vector
+void Matriu_x_Vector( float M[N][N], float vect[N], float vectres[N] ) {
+	for (int i=0;i<N;i++){
+		for (int j=0;j<N;j++){
+			vectres[i]=Scalar( M[i], vect);
+		}
+	}
+}
 
 //MAIN
 int main(){
@@ -156,7 +158,9 @@ int main(){
 	//printf("%f \n",Scalar( V1,V3 ));
 	//printf("%d \n",Ortogonal( V1,V2 ));
 	//printf("%f \n",Infininorm(Mat));
-	printf("%f \n",NormFrobenius(Mat));
+	// printf("%f \n",NormFrobenius(Mat));
+	PrintVect(Matriu_x_Vector(Mat,V2,V4),0,10);
+
 }
 
 
